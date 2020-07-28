7ec61cc27a6ea831590094f3bd4a472a9b3cfa51
Rafael Amorim <60046885+RatsuGamer@users.noreply.github.com>
2020-07-28 00:46:10 -0300

    Update ex 72.py

--- ex 72.py
+++ ex 72.py
@@ -1,5 +1,7 @@
+# Desafio 72: Número por Extenso
 # Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
 
 cont = ('zero', 'um', 'dois', 'três' , 'quatro', 'cinco', 'seis',
 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze',
