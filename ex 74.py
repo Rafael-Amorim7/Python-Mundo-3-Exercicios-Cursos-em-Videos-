60fb1e9b828b3b35df23511b1a6a5070ac70fe50
Rafael Amorim <60046885+RatsuGamer@users.noreply.github.com>
2020-07-28 00:43:54 -0300

    Add files via upload

---
+++ ex 74.py
@@ -0,0 +1,13 @@
+# Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
+# Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+
+from random import randint
+
+num = (randint(0,10),randint(0,10),randint(0,10),randint(0,10),randint(0,10))
+
+print(f'Os valores sorteados foram: ', end='')
+for i in num:
+    print(f'{i}', end=' ')
+
+print(f'\nO maior: {max(num)}')
+print(f'O menor: {min(num)}')
