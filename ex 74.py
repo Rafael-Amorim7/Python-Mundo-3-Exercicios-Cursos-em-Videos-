d9c0a86e9ddd109a7f7bc5c028ef8aa46f1a3bfa
Rafael Amorim <60046885+RatsuGamer@users.noreply.github.com>
2020-07-28 00:47:04 -0300

    Update ex 74.py

--- ex 74.py
+++ ex 74.py
@@ -1,3 +1,4 @@
+# Desafio 74: Maior e menor valores em tuplas
 # Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
 # Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
 
